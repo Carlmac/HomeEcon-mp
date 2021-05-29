cd549d60d382990415345abb31d9e0e1079e18e9
carlmac <carlmac@163.com>
2021-05-29 21:40:34 +0800

    添加 tab-bar 点击切换样式功能

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,15 @@
 Page({
-  data: {},
+  data: {
+    tabs: ['全部服务', '在提供', '正在找'],
+    currentTabIndex: 0
+  },
   onLoad: function (options) {
 
+  },
+  handleTabChange: function(e) {
+    const index = e.currentTarget.dataset.index;
+    this.setData({
+      currentTabIndex: index
+    });
   }
 });
