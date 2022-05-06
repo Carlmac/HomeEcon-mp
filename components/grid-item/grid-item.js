310118e65ddb76bf4dce93c1383be0e75924e4e4
carlmac <carlmac@163.com>
2022-05-06 23:11:03 +0800

    more code

---
+++ components/grid-item/grid-item.js
@@ -0,0 +1,11 @@
+Component({
+  properties: {
+    icon: String,
+    iconSize: String,
+    text: String,
+    showBadge: Boolean,
+    badgeCount: Number,
+  },
+  data: {},
+  methods: {}
+});
---
+++ components/grid/grid.js
@@ -0,0 +1,12 @@
+Component({
+  properties: {
+    rowNum: {
+      type: Number,
+      value: 3,
+    },
+    title: String,
+    extend: String,
+  },
+  data: {},
+  methods: {}
+});
